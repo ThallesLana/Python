6a1ef78b083c41346329b156b0f2774e73f93b4a
ThallesLana <Thalles.lana1@gmail.com>
2021-02-01 21:47:55 -0300

    Guanabara me salvando nas matrizes

---
+++ PROJETOS/guppe/Guanabara/matrizes.py
@@ -0,0 +1,17 @@
+"""
+Matriz
+
+"""
+
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input(f'Digite um valor [{l}, {c}]: '))
+print('-=' * 30)
+
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]}]', end='')
+    print()
+
